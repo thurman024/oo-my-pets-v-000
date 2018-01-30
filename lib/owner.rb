ef1dd3f61fb9c55d8bdd3a53c9d53f969d112080
Stephen Thurman <thurman024@gmail.com>
2018-01-30 23:36:02 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,14 +1,24 @@
 class Owner
   attr_accessor :pets
-  @@all_owners = 0
+  @@all_owners = []
+  @@owner_count = 0
   def initialize
     @pets = {fishes: [], cats: [], dogs: []}
-    @@all_owners += 1
+    
+  end
+
+  #Class methods
+  def self.all
+    @@all_owners
+  end
+
+  def self.count
+    @@owner_count
   end
 
   def buy_fish(fish_name)
     new_fish = Fish.new(fish_name)
     self.pets[:fishes] << new_fish
-    
+
 
 end
